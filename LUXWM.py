2279cabb8fe2c6b6f1862cc3bf89876721f77064
Yourong Wang <44557467+Sifyrena@users.noreply.github.com>
2023-09-21 01:01:51 +1200

    Version 3
    Added fancy borders and disabled extra padding by default.

--- LUXWM.py
+++ LUXWM.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageDraw, ImageFont, ExifTags, ImageOps
+from PIL import Image, ImageDraw, ImageFont, ExifTags, ImageOps, ImageFilter
 from colorthief import ColorThief
 import blend_modes as bm
 import numpy as np
@@ -9,6 +9,8 @@ from PIL.ExifTags import TAGS
 import matplotlib.pyplot as plt
 import matplotlib
 
+import argparse 
+
 matplotlib.rcParams.update({'font.size': 18, 'font.family': "Avenir Next"})
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -16,18 +18,267 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import os
 from os import listdir
 
+def hex_to_rgb(hex_color):
+    # Remove the '#' character if present
+    hex_color = hex_color.lstrip('#')
+    
+    # Ensure the hex color is 6 characters long
+    if len(hex_color) != 6:
+        raise ValueError("Invalid hex color value")
+    
+    # Extract the R, G, and B components
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+    
+    return r, g, b
+
+
+def add_border_fancy(
+        imagepath,
+        Label=[],
+        Social = True,
+        Print_Data = True,
+        Shuffle_Dominant=False,
+        Shuffle_Options=4,
+        Pad_Extra=False,
+        padding_ratio=0.01,
+        WMPath = "",
+        ColorOverride = "",
+        GaussianRad = 100,
+        BorderWidth = 25):
+
+    image = Image.open(imagepath)
+
+    image = ImageOps.exif_transpose(image)
+
+    width, height = image.size
+
+    if Social and (width > 3000 or height > 3000):
+
+        print("Resizing for social media.")
+
+        if height > width:
+            ratio = width / height
+            height = 3000
+            width = int(height * ratio)
+
+        else:
+            ratio = height / width
+            width = 3000
+            height = int(width * ratio)
+
+        image = image.resize((width, height))
+
+    width, height = image.size
+    #print(width, height)
+
+    if ColorOverride == "":
+
+        color_thief = ColorThief(imagepath)
+        # get the dominant color
+
+        if Shuffle_Dominant:
+
+            colors = color_thief.get_palette(color_count=Shuffle_Options, quality=500)
+
+            dominant_color = colors[np.random.randint(Shuffle_Options)]
+
+        else:
+
+            dominant_color = color_thief.get_color(quality=500)
+
+        dom_R, dom_G, dom_B = dominant_color
+
+    else: 
+        print(f"Overriding the Border Color with User-Specified {ColorOverride}")
+
+
+
+        dom_R, dom_G, dom_B = LUXWM.hex_to_rgb(ColorOverride)
+        dominant_color = (dom_R, dom_G, dom_B)
+    #if Quiet:
+
+    #else:
+    #    WMPath = input("Type in the code for watermark series, enter to use default.")
+
+    if WMPath == "":
+
+        WMFile = "./Private/_Mark"
+
+    elif (WMPath == "VAR") or (WMPath == "Custom"):
+        WMTempName = input("Choose a Watermark")
+
+        if WMTempName == "":
+            WMFile = "./Private/_Mark"
+        else:
+            WMFile = "./Private/LUXMarks/" + WMTempName
+
+    else:
+        WMFile = "./Private/LUXMarks/" + WMPath
+
+
+
+    if (dom_R * 0.299 + dom_G * 0.587 + dom_B * 0.114) > 150:  #186:
+        print("Light")
+
+        try:
+            Watermark = Image.open(WMFile+"D.png")
+        except:
+            Watermark = Image.new('RGB', (200, 200), (0, 0, 0))
+        Method = bm.multiply
+
+        Border = (0,0,0)
+
+        Light = True
+
+    else:
+        print("Dark")
+        try:
+            Watermark = Image.open(WMFile+"W.png")
+        except:
+            Watermark = Image.new('RGB', (200, 200), (255, 255, 255))
+        Method = bm.screen
+
+        Border = (255,255,255)
+
+        Light = False
+
+    # Go!
+    dim = max(height, width)
+    wmsize = dim / 4
+    if width / height >= 2:
+        offset = (0, (dim - height) / 2)
+        wmoffset = (0, dim - wmsize)
+        infooffset = (dim - 2 * wmsize, dim - wmsize)
+
+    else:
+        dim = height + wmsize * 2  #
+        offset = ((dim - width) / 2, wmsize)
+
+        if width / height >= 3 / 4:
+
+            wmoffset = ((dim - width) / 2, wmsize + height)
+            infooffset = ((dim + width) / 2 - 2 * wmsize, wmsize + height)
+
+        else:
+            Pad_Extra = False
+            wmsizeOld = wmsize
+            wmsize = width / 3
+            wmoffset = ((dim - width) / 2, wmsizeOld + height)
+            infooffset = ((dim - width) / 2 + wmsize, wmsizeOld + height)
+
+    # Convert to Integers
+    dim = int(dim)
+    wmsize = int(wmsize)
+    offset = tuple(int(item) for item in offset)
+    foffset = tuple(int(item - BorderWidth//2) for item in offset)
+    wmoffset = tuple(int(item) for item in wmoffset)
+    infooffset = tuple(int(item) for item in infooffset)
+
+    # Create Blurry Version of Original Image
+
+    blurred_image = image.filter(ImageFilter.GaussianBlur(radius = GaussianRad))
+
+    # Keep Central Square
+
+    mindim = min(width, height)
+
+    # Create a blank square image
+    square_image = Image.new('RGB', (mindim, mindim), (255, 255, 255))  # You can change the background color if needed
+
+    # Calculate the position to paste the original image in the center
+    x_offset = (mindim - width) // 2
+    y_offset = (mindim - height) // 2
+
+    # Paste the original image onto the square image
+    square_image.paste(blurred_image, (x_offset, y_offset))
+
+    new_image = square_image.resize((dim, dim))
+
+    frame = Image.new("RGB", (width + BorderWidth, height + BorderWidth), Border)
+
+    #new_image = Image.new("RGB", (dim, dim), dominant_color)
+    new_image.paste(frame, foffset)
+    new_image.paste(image, offset)
+
+    print("Watermark Size", wmsize)
+
+
+
+    Watermark = Watermark.resize((wmsize, wmsize))
+    new_image.paste(Watermark, wmoffset, Watermark)
+    if Label != []:
+        LabelImage = Image.new("RGB", (dim, dim), (0, 0, 0))
+
+        Lwidth, Lheight = LabelImage.size
+
+        Lheight = wmsize
+        Lwidth = 2 * wmsize
+
+        Label = Label.resize((Lwidth, Lheight))
+
+        LabelImage.paste(Label, infooffset)
+
+        if Light:
+            LabelImage = ImageOps.invert(LabelImage)
+
+        alpha = Image.new('L', new_image.size, 255)
+
+        new_image.putalpha(alpha)
+        #new_image.show()
+        LabelImage.putalpha(alpha)
+        #LabelImage.show()
+
+        background_img = np.array(new_image).astype(float)
+        foreground_img = np.array(LabelImage).astype(float)
+
+        # Blend images
+        opacity = 1  # The opacity of the foreground that is blended onto the background is 70 %.
+
+        if Print_Data:
+            blended_img_float = Method(background_img, foreground_img, opacity)
+
+            # Convert blended image back into PIL image
+            blended_img = np.uint8(
+                blended_img_float
+            )  # Image needs to be converted back to uint8 type for PIL handling.
+            new_image = Image.fromarray(
+                blended_img
+            )  # Note that alpha channels are displayed in black by PIL by default.
+            # This behavior is difficult to change (although possible).
+            # If you have alpha channels in your images, then you should give
+            # OpenCV a try.
+
+    if Pad_Extra:
+
+        dimADL = int(dim * padding_ratio)
+
+        final_image = Image.new("RGB",
+                                (dim + dimADL + dimADL, dim + dimADL + dimADL),
+                                dominant_color)
+        final_image.paste(new_image, (dimADL, dimADL))
+
+
+
+        return final_image
+
+    else:
+
+        return new_image
+
 
 def add_border(
         imagepath,
         Label=[],
         Social = True,
-        watermark_ratio=1,
-        Adaptive=True,  # If Adaptive, expand border for image watermarks. If not, tall images will be watermarked sideways
-        watermark_ratio_max=0.3,  # Times Image Max Dimension
-        Shuffle_Dominant=True,
+        Print_Data = True,
+        Shuffle_Dominant=False,
         Shuffle_Options=4,
-        Pad_Extra=True,
-        padding_ratio=0.01):
+        Pad_Extra=False,
+        padding_ratio=0.01,
+        WMPath = "",
+        ColorOverride = ""):
 
     image = Image.open(imagepath)
 
@@ -54,26 +305,53 @@ def add_border(
     width, height = image.size
     #print(width, height)
 
-    color_thief = ColorThief(imagepath)
-    # get the dominant color
+    if ColorOverride == "":
 
-    if Shuffle_Dominant:
+        color_thief = ColorThief(imagepath)
+        # get the dominant color
 
-        colors = color_thief.get_palette(color_count=Shuffle_Options, quality=500)
+        if Shuffle_Dominant:
 
-        dominant_color = colors[np.random.randint(Shuffle_Options)]
+            colors = color_thief.get_palette(color_count=Shuffle_Options, quality=500)
+
+            dominant_color = colors[np.random.randint(Shuffle_Options)]
+
+        else:
+
+            dominant_color = color_thief.get_color(quality=500)
+
+        dom_R, dom_G, dom_B = dominant_color
+
+    else: 
+        print(f"Overriding the Border Color with User-Specified {ColorOverride}")
+
+        
+
+        dom_R, dom_G, dom_B = hex_to_rgb(ColorOverride)
+        dominant_color = (dom_R, dom_G, dom_B)
+    #if Quiet:
+    
+    #else:
+    #    WMPath = input("Type in the code for watermark series, enter to use default.")
+
+    if WMPath == "":
+
+        WMFile = "./Private/_Mark"
+
+    elif (WMPath == "VAR") or (WMPath == "Custom"):
+        WMTempName = input("Choose a Watermark")
+        WMFile = "./Private/LUXMarks/" + WMTempName
 
     else:
+        WMFile = "./Private/LUXMarks/" + WMPath
 
-        dominant_color = color_thief.get_color(quality=500)
 
-    dom_R, dom_G, dom_B = dominant_color
 
     if (dom_R * 0.299 + dom_G * 0.587 + dom_B * 0.114) > 150:  #186:
         print("Light")
         
         try:
-            Watermark = Image.open("./Private/_MarkD.png")
+            Watermark = Image.open(WMFile+"D.png")
         except:
             Watermark = Image.new('RGB', (200, 200), (0, 0, 0))
         Method = bm.multiply
@@ -83,7 +361,7 @@ def add_border(
     else:
         print("Dark")
         try:
-            Watermark = Image.open("./Private/_MarkW.png")
+            Watermark = Image.open(WMFile+"W.png")
         except:
             Watermark = Image.new('RGB', (200, 200), (255, 255, 255))
         Method = bm.screen
@@ -156,18 +434,20 @@ def add_border(
 
         # Blend images
         opacity = 1  # The opacity of the foreground that is blended onto the background is 70 %.
-        blended_img_float = Method(background_img, foreground_img, opacity)
+        
+        if Print_Data:
+            blended_img_float = Method(background_img, foreground_img, opacity)
 
-        # Convert blended image back into PIL image
-        blended_img = np.uint8(
-            blended_img_float
-        )  # Image needs to be converted back to uint8 type for PIL handling.
-        new_image = Image.fromarray(
-            blended_img
-        )  # Note that alpha channels are displayed in black by PIL by default.
-        # This behavior is difficult to change (although possible).
-        # If you have alpha channels in your images, then you should give
-        # OpenCV a try.
+            # Convert blended image back into PIL image
+            blended_img = np.uint8(
+                blended_img_float
+            )  # Image needs to be converted back to uint8 type for PIL handling.
+            new_image = Image.fromarray(
+                blended_img
+            )  # Note that alpha channels are displayed in black by PIL by default.
+            # This behavior is difficult to change (although possible).
+            # If you have alpha channels in your images, then you should give
+            # OpenCV a try.
 
     if Pad_Extra:
 
@@ -299,12 +579,16 @@ def Exif_to_Digest(imagepath, Quiet, Message):
 
     return figure_to_image(fig)
 
-def Process_Batch(folder,
+def Process_Batch(folder = "./Inputs",
                   Social=True,
                   Threshold=3000,
                   Silent=True,
+                  Fancy=False,
                   SeriesTitle="",
-                  Idempotence=True):
+                  ColorOverride="",
+                  Idempotence=True,
+                  Print_Data = True,
+                  WMPath = ""):
 
     Quiet = Silent
     for imagefile in sorted(os.listdir(folder)):
@@ -323,9 +607,35 @@ def Process_Batch(folder,
             Label = Exif_to_Digest(folder + "/" + imagefile, Quiet, SeriesTitle)
 
             width, height = image.size
+            
+            if Fancy:
+                
+                image = add_border_fancy(folder + "/" + imagefile, Label, Social, Print_Data=Print_Data, WMPath = WMPath, ColorOverride = ColorOverride)
+                
+            else:
 
-            image = add_border(folder + "/" + imagefile, Label, Social)
+                image = add_border(folder + "/" + imagefile, Label, Social, Print_Data=Print_Data, WMPath = WMPath, ColorOverride = ColorOverride)
 
             image = image.convert('RGB')
 
             image.save(f"{folder}/Processed_{imagefile}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process a batch using the Python function')
+    parser.add_argument('folder', type=str, default = "./Inputs", help='The folder to process')
+    parser.add_argument('--Social', action='store_true', default=True, help='Enable Social Resizing (Default: 3000px)')
+    parser.add_argument('--Threshold', type=int, default=3000, help='Threshold value')
+    parser.add_argument('--Silent', action='store_true', help='Enable Silent')
+    parser.add_argument('--Fancy', action='store_true', help='Enable Fancy Blurry')
+    parser.add_argument('--SeriesTitle', type=str, default='', help='Series Title')
+    parser.add_argument('--ColorOverride', type=str, default='', help='Override Color')
+    parser.add_argument('--Idempotence', action='store_false', default=True, help='Enable Idempotence')
+    parser.add_argument('--PrintData', action='store_false', default=True, help='Enable Print_Data')
+    parser.add_argument('WM', default = "", type = str, help='Select Watermark Family')
+    args = parser.parse_args()
+
+    Process_Batch(args.folder, args.Social, args.Threshold, args.Silent, args.Fancy, args.SeriesTitle, args.ColorOverride, args.Idempotence, args.PrintData, args.WM)
+
+if __name__ == '__main__':
+    main()
